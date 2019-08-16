1892320bdeb3c8d5579c15e9e5c0724fe11a6255
Julien <jconan@outlook.fr>
2019-08-16 13:06:09 +0200

    Devblog update

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/mnt/c/Users/jcon/Documents/DEV/devblog/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/mnt/c/Users/jcon/Documents/DEV/devblog/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"UA-131377558-1"},
-    }]
